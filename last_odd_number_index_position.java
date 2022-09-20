d3f721e5584ff5ecffd5814c352d14b7357e4e04
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-09-20 19:51:42 +0530

    import java.util.Scanner; class last {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[],i;         b=new int[a];         for(i=0;i<a;i++)         {             b[i]=sc.nextInt();         }         for(i=a-1;i>=0;i--)         {             if(b[i]%2!=0)             {                 System.out.println(i);                 break;             }         }
        }
    }

---
+++ last_odd_number_index_position.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class last
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[],i;
+        b=new int[a];
+        for(i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        for(i=a-1;i>=0;i--)
+        {
+            if(b[i]%2!=0)
+            {
+                System.out.println(i);
+                break;
+            }
+        }
+        
+    }
+}
