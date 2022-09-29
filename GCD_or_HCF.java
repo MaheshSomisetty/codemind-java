d79369b440b1c6e30f1ed5e79c777e8fe00f8883
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-09-29 20:21:50 +0530

    import java.util.Scanner; class gcd{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a,b,i;         a=sc.nextInt();         b=sc.nextInt();         for(i=a;i>0;i--){             if(a%i==0 && b%i==0){                 break;             }         }         System.out.println(i);     } }

---
+++ GCD_or_HCF.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class gcd{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a,b,i;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=a;i>0;i--){
+            if(a%i==0 && b%i==0){
+                break;
+            }
+        }
+        System.out.println(i);
+    }
+}
