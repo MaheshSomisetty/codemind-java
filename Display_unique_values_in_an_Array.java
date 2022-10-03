3e3bba99eb0abc5e902084433cbf20355ef7b871
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-03 13:30:08 +0530

    import java.util.*; class unique {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {             b[i]=sc.nextInt();         }         ArrayList<Integer> al=new ArrayList<Integer>();         ArrayList<Integer> al1=new ArrayList<Integer>();         ArrayList<Integer> al2=new ArrayList<Integer>();         for(int ele:b)         {             al.add(ele);         }         for(Integer ele:al)         {             if(Collections.frequency(al,ele)==1)             {                 al1.add(ele);             }         }         if(al1.isEmpty())         {             System.out.println(-1);         }         else         {             for(int ele:al1)             {                 System.out.print(ele+" ");             }         }     } }

---
+++ Display_unique_values_in_an_Array.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class unique
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        ArrayList<Integer> al1=new ArrayList<Integer>();
+        ArrayList<Integer> al2=new ArrayList<Integer>();
+        for(int ele:b)
+        {
+            al.add(ele);
+        }
+        for(Integer ele:al)
+        {
+            if(Collections.frequency(al,ele)==1)
+            {
+                al1.add(ele);
+            }
+        }
+        if(al1.isEmpty())
+        {
+            System.out.println(-1);
+        }
+        else
+        {
+            for(int ele:al1)
+            {
+                System.out.print(ele+" ");
+            }
+        }
+    }
+}
