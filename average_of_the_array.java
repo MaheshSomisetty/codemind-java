e2cad06da2aa7c9d97dd778593e3a616bc7b1eb8
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-07 20:57:54 +0530

    import java.util.*; class average {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[]=new int[a];         int c=0,d=0;         for(int i=0;i<a;i++)         {             b[i]=sc.nextInt();             c+=1;             d+=b[i];         }         float e=(float)d/c;         System.out.printf("%.2f",e);
        }
    }

---
+++ average_of_the_array.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class average
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        int c=0,d=0;
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+            c+=1;
+            d+=b[i];
+        }
+        float e=(float)d/c;
+        System.out.printf("%.2f",e);
+        
+        
+    }
+}
