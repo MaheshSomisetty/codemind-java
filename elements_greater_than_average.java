bd2bd9bb2a4d18b04a44f4cdd2813d7501ac15f4
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-07 20:28:32 +0530

    import java.util.*; class average {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[]=new int[a];         int c=0,d=0;         for(int i=0;i<a;i++)         {             b[i]=sc.nextInt();             c+=1;             d+=b[i];         }         int e=d/c;         int x=0;         for(int ele:b)         {             if(ele>=e)             {                 x+=1;             }         }         System.out.print(x);
        }
    }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class average
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        int c=0,d=0;
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+            c+=1;
+            d+=b[i];
+        }
+        int e=d/c;
+        int x=0;
+        for(int ele:b)
+        {
+            if(ele>=e)
+            {
+                x+=1;
+            }
+        }
+        System.out.print(x);
+        
+    }
+}
