eab18685b552300aabbc3ce0beff5dcbcad1cdac
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-07 17:07:03 +0530

    import java.util.*; class minimum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {             b[i]=sc.nextInt();         }         int c=sc.nextInt();         int d=sc.nextInt();         ArrayList<Integer> al=new ArrayList<Integer>();         for(int ele:b)         {             if(ele>=c && ele<=d)             {                 al.add(ele);             }
            }
            if(al.isEmpty())
            {
                System.out.print(-1);
            }
            else
            {
                System.out.print(Collections.min(al));
            }
        }
    }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class minimum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        ArrayList<Integer> al=new ArrayList<Integer>();
+        for(int ele:b)
+        {
+            if(ele>=c && ele<=d)
+            {
+                al.add(ele);
+            }
+            
+        }
+        if(al.isEmpty())
+        {
+            System.out.print(-1);
+        }
+        else
+        {
+            System.out.print(Collections.min(al));
+        }
+    }
+}
