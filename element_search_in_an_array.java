c50bbc3bcc9191a792fb460bdbf86a74d307009e
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-07 20:52:46 +0530

    import java.util.Scanner; class count {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b[]=new int[a];         for(int i=0;i<a;i++)         {             b[i]=sc.nextInt();         }         int c=sc.nextInt();         int d=0;         for(int i=0;i<a;i++)         {             if(b[i]==c)             {                 d=1;                 break;             }         }         if(d==0)         {             System.out.print("False");         }         else         {             System.out.print("True");         }
        }
    }

---
+++ element_search_in_an_array.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class count
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            b[i]=sc.nextInt();
+        }
+        int c=sc.nextInt();
+        int d=0;
+        for(int i=0;i<a;i++)
+        {
+            if(b[i]==c)
+            {
+                d=1;
+                break;
+            }
+        }
+        if(d==0)
+        {
+            System.out.print("False");
+        }
+        else
+        {
+            System.out.print("True");
+        }
+        
+    }
+}
