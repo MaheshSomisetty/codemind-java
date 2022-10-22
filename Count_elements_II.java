06c96776fdace8624db0cc5b30824528cd0882f3
MaheshSomisetty <97424705+MaheshSomisetty@users.noreply.github.com>
2022-10-22 14:06:53 +0530

    import java.util.*; class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         int [] arr1=new int [n];         int [] arr2=new int [m];         for (int i=0;i<n;i++)             arr1[i]=sc.nextInt();         for (int i=0;i<m;i++)             arr2[i]=sc.nextInt();         HashSet<Integer> a1=new HashSet<Integer>();         for (int ele:arr1)             a1.add(ele);         HashSet<Integer> a2=new HashSet<Integer>();         for (int ele:arr2)             a2.add(ele);         int c=0;         for (int ele:a1){             if (a2.contains(ele)==false)                 c+=1;         }         for (int ele:a2){             if (a1.contains(ele)==false)                 c+=1;         }         System.out.print(c);     } }

---
+++ Count_elements_II.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int [] arr1=new int [n];
+        int [] arr2=new int [m];
+        for (int i=0;i<n;i++)
+            arr1[i]=sc.nextInt();
+        for (int i=0;i<m;i++)
+            arr2[i]=sc.nextInt();
+        HashSet<Integer> a1=new HashSet<Integer>();
+        for (int ele:arr1)
+            a1.add(ele);
+        HashSet<Integer> a2=new HashSet<Integer>();
+        for (int ele:arr2)
+            a2.add(ele);
+        int c=0;
+        for (int ele:a1){
+            if (a2.contains(ele)==false)
+                c+=1;
+        }
+        for (int ele:a2){
+            if (a1.contains(ele)==false)
+                c+=1;
+        }
+        System.out.print(c);
+    }
+}
